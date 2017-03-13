3a3dd6a6bd5716c78b8ccdf16d8312a3ad15de9d
Vamshedhar Reddy <vamshedhar@gmail.com>
2017-03-12 21:46:21 -0400

    feat(encoder): add validations and comments

--- Encoder.java
+++ Encoder.java
@@ -4,9 +4,11 @@
 */
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
@@ -15,50 +17,86 @@ import java.util.HashMap;
 public class Encoder {
 	
 	private static BufferedReader inputStream;
+	private static FileOutputStream outputStream;
 
 	public static void main(String[] args) throws IOException {
+
+		// Input format validations
+        if (args.length != 2) {
+            System.out.println("Invalid Format!");
+            System.out.println("Expected Format: java Encoder <filename or filepath> <bitlength>");
+            return;
+        }
 		
 		String filename = args[0];
-		double BIT_LENGTH = Double.parseDouble(args[1]);
-		int MAX_TABLE_SIZE = (int) Math.pow(2.0, BIT_LENGTH);
+		int BIT_LENGTH;
+
+		// check if bit length is a valid integer
+		try{
+			BIT_LENGTH = Integer.parseInt(args[1]);
+		} catch(NumberFormatException e){
+			System.out.println("Invalid bit length!");
+			return;
+		}
 		
-		HashMap<String, Integer> encoding_table = new HashMap<String, Integer>(); 
+		int MAX_TABLE_SIZE = (int) Math.pow(2, BIT_LENGTH);
+
+		// Initialize encoding table with all ASCII characters, with character as key and ASCII code as its value
+		HashMap<String, Integer> ENCODING_TABLE = new HashMap<String, Integer>(); 
 		
 		for(int i = 0; i < 256; i++){
-			encoding_table.put(Character.toString((char) i), i);
+			ENCODING_TABLE.put(Character.toString((char) i), i);
 		}
-		
+
 		int TABLE_SIZE = 256;
-		
-		int pos = filename.lastIndexOf(".");
-		String fileTitle = filename.substring(0, pos);
-		
-		String stringToHash = "";
-		
-		FileOutputStream outputStream = new FileOutputStream(fileTitle+ ".lzw");
-		Writer writer = new OutputStreamWriter(outputStream, Charset.forName("UTF-16BE"));
-		
-		inputStream = new BufferedReader(new FileReader(filename));
-		int c = 0;
-		
-		while ((c = inputStream.read()) != -1) {
-			char ch = (char) c;
-			if(encoding_table.containsKey(stringToHash + ch)){
-				stringToHash = stringToHash + ch;
-			} else{
-//				System.out.println(encoding_table.get(stringToHash));
-				writer.write(encoding_table.get(stringToHash));
-				if(TABLE_SIZE < MAX_TABLE_SIZE){
-					encoding_table.put(stringToHash + ch, TABLE_SIZE++);
+
+		// check if its a valid file
+		try{
+
+			// Encoding starts here. Reads charactes from input file, stores new codes to table and writes to encoded life
+			inputStream = new BufferedReader(new FileReader(filename));
+			int c = 0;
+
+			// get file title from name
+			int pos = filename.lastIndexOf(".");
+			String fileTitle = filename.substring(0, pos);
+			
+			String stringToHash = "";
+			
+			// write file in UTF-16BE format
+			outputStream = new FileOutputStream(fileTitle+ ".lzw");
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, Charset.forName("UTF-16BE")));
+			
+			while ((c = inputStream.read()) != -1) {
+				char ch = (char) c;
+
+				// check if code exists in the table
+				if(ENCODING_TABLE.containsKey(stringToHash + ch)){
+					stringToHash = stringToHash + ch;
+				} else{
+
+					// write to encoded file
+					writer.write(ENCODING_TABLE.get(stringToHash));
+
+					// check if the table size has not reached its limit
+					if(TABLE_SIZE < MAX_TABLE_SIZE){
+						// store new codes to the table and increment table size
+						ENCODING_TABLE.put(stringToHash + ch, TABLE_SIZE++);
+					}
+					stringToHash = Character.toString(ch);
 				}
-				stringToHash = Character.toString(ch);
 			}
+			
+			// write the encoded part of the remaning part of string to table 
+			writer.write(ENCODING_TABLE.get(stringToHash));
+			writer.flush();
+
+		} catch(FileNotFoundException e){
+			System.out.println("File not found. Please give a valid filename/filepath.");
+			return;
 		}
 		
-//		System.out.println(encoding_table.get(stringToHash));
-		writer.write(encoding_table.get(stringToHash));
-		writer.flush();
-		
+		// close the file reader and writer
 		inputStream.close();
 		outputStream.close();
 	}
